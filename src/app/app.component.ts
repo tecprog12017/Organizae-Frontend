1a2691c99798ee647a1019f0239ff75b602e2627
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-03-29 01:04:43 -0300

    Creating the sign up page and making it the home page of the application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,15 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+//import { TabsPage } from '../pages/tabs/tabs';
+import { SignUp } from '../pages/user/signUp/sign-up.component';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TabsPage;
+  rootPage = SignUp;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
