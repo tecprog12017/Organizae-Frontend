ad4411078189c8176dfee3881c758913fb6f5603
Lucas Oliveira <lucasoliveira2306@gmail.com>
2017-03-30 12:53:34 -0300

    Refactoring some variables and submiting form to backend.
    Signed-off-by: Matheus Joranhezon <mjoranhezon@gmail.com>
    Signed-off-by: Lucas Olivera <lucasoliveira2306@gmail.com>
    Signed-off-by: Fabiola Fleury <fabiola.mfleury@gmail.com>
    Signed-off-by: Hugo Carvalho <hugo-nc@hotmail.com>

--- src/pages/user/signUp/sign-up.component.ts
+++ src/pages/user/signUp/sign-up.component.ts
@@ -1,27 +1,33 @@
 import { Component} from "@angular/core";
 import { NavController } from "ionic-angular"
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { UserProfile } from '../../../models/userProfile';
+import { Http } from '@angular/http';
 
 @Component({
   selector: "sign-up",
   templateUrl: 'sign-up.component.html'
 })
 
-export class SignUp {
-  authForm: FormGroup;
 
-  constructor(public navCtrl: NavController, fb: FormBuilder) {
-    this.authForm = fb.group({
+export class SignUp {
+  signUpForm: FormGroup;
+  newUser: UserProfile;
+
+  //Creating responsive for user profile sign up.
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder, private http: Http) {
+    this.signUpForm = formBuilder.group({
       'firstName' : [null, Validators.compose([Validators.required, Validators.maxLength(10)])],
       'lastName' : [null, Validators.compose([Validators.required])],
       'email' : [null, Validators.compose([Validators.required])],
       'password' : [null, Validators.compose([Validators.required])]
-    })
+    });
   }
 
+  //Creating method to submit form values to backend
   submitForm(value: any):void{
-    console.log('Form Submitted!');
-    console.log(value);
+    this.newUser = new UserProfile(this.signUpForm);
+    this.http.post('localhost:3000/api/UserProfile/sign-up', this.newUser);
   }
 
 }
