a43e1e16859ebdbd6fe02643f9936a0658ee0fe6
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-03-31 14:30:19 -0300

    Creating class for validations. Email and password were validated.
    Signed-off-by: Matheus Joranhezon <mjoranhezon@gmail.com>
    Signed-off-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

---
+++ src/controller/custom-validations.ts
@@ -0,0 +1,33 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+//Validation used for the email on the user account
+export function ValidateEmail (): ValidatorFn {
+
+  return (control: AbstractControl): {[key: string]: any} => {
+    const emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    var isValid = emailRegex.test(control.value);
+
+    if (isValid){
+      return null;
+    }
+    else {
+      return { ValidateEmailOutput: true};
+    }
+  };
+}
+
+//Validation used for the password on the user account
+export function ValidatePassword (): ValidatorFn {
+
+  return (control: AbstractControl): {[key: string]: any} => {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+    var isValid = passwordRegex.test(control.value);
+
+    if (isValid){
+      return null;
+    }
+    else {
+      return { ValidatePasswordOutput: true};
+    }
+  }
+}
