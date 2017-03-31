2046d1df477b4d7f7ad6da321eb881d3371aacab
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-03-31 14:30:19 -0300

    Creating class for validations. Email and password were validated.
    Signed-off-by: Matheus Joranhezon <mjoranhezon@gmail.com>
    Signed-off-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

--- src/pages/user/signUp/sign-up.component.ts
+++ src/pages/user/signUp/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component} from "@angular/core";
 import { NavController } from "ionic-angular"
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { UserProfile } from '../../../models/userProfile';
+import { ValidateEmail, ValidatePassword } from '../../../controller/custom-validations'
 import { Http } from '@angular/http';
 
 @Component({
@@ -15,19 +16,19 @@ export class SignUp {
   newUser: UserProfile;
 
   //Creating responsive for user profile sign up.
-  constructor(public navCtrl: NavController, private formBuilder: FormBuilder, private http: Http) {
+  constructor(public navCtrl: NavController, formBuilder: FormBuilder, private http: Http) {
     this.signUpForm = formBuilder.group({
-      'firstName' : [null, Validators.compose([Validators.required, Validators.maxLength(10)])],
-      'lastName' : [null, Validators.compose([Validators.required])],
-      'email' : [null, Validators.compose([Validators.required])],
-      'password' : [null, Validators.compose([Validators.required])]
+      'firstName' : [null, Validators.compose([Validators.required, Validators.maxLength(15)])],
+      'lastName' : [null, Validators.compose([Validators.required, Validators.maxLength(15)])],
+      'email' : [null, Validators.compose([Validators.required, ValidateEmail()])],
+      'password' : [null, Validators.compose([Validators.required, ValidatePassword()])]
     });
   }
 
   //Creating method to submit form values to backend
   submitForm(value: any):void{
     this.newUser = new UserProfile(this.signUpForm);
-    this.http.post('localhost:3000/api/UserProfile/sign-up', this.newUser);
+    this.http.post('http://localhost:3000/api/UserProfiles/sign-up', this.newUser).subscribe();
   }
 
 }
