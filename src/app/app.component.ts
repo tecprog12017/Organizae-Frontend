bc96d4867df15022cb9ddf038f57dfa377607eff
hugo-nc <hugo-nc@hotmail.com>
2017-04-13 08:17:54 -0300

    Added cpf imports
    Signed-off-by: luis-gustavo <luis.gustavo3013@hotmail.com>
    Signed-off-by: hugo-nc <hugo-nc@hotmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 //import { TabsPage } from '../pages/tabs/tabs';
 import { SignUp } from '../pages/user/signUp/sign-up.component';
-import { Edit } from '../pages/user/edit/editGender/editGender.component';
+import { EditCpf } from '../pages/user/edit/editCpf/editCpf.component';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = Edit;
+  rootPage = EditCpf;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,7 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { SignUp } from '../pages/user/signUp/sign-up.component';
-import { Edit } from '../pages/user/edit/editGender/editGender.component'
-
+import { EditCpf } from '../pages/user/edit/editCpf/editCpf.component'
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -19,7 +18,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     TabsPage,
     SignUp,
-    Edit
+    EditCpf
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -32,7 +31,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage,
     TabsPage,
     SignUp,
-    Edit
+    EditCpf
   ],
   providers: [
     StatusBar,
