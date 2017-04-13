41a633ccb808e298c8aff31321feaf2c6a80b7c2
Hugo Neves de Carvalho <hugo-nc@hotmail.com>
2017-04-13 11:00:25 -0300

    Created form to user edit rg
    Signed-off-by: Hugo Neves de Carvalho <hugo-nc@hotmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

---
+++ src/pages/user/edit/editRg/editRg.component.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular"
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { UserProfile, Rg } from '../../../../models/userProfile';
+import { ValidateEmail, ValidatePassword } from '../../../controller/custom-validations'
+import { Http } from '@angular/http'
+
+@Component({
+  selector: "edit",
+  templateUrl: 'editRg.component.html'
+})
+
+export class EditRg {
+  editRgForm: FormGroup;
+  newRg: Rg;
+
+  //Creating responsive for user profile edit gender.
+  constructor(public navCtrl: NavController, formBuilder: FormBuilder, private http: Http) {
+    this.editRgForm = formBuilder.group({
+      'rgNumber' : [null, Validators.compose([Validators.required])],
+      'rgExpeditionState' : [null, Validators.compose([Validators.required])]
+    });
+  }
+
+  //Creating method to submit form gender values to backend
+  submitForm(value: any):void{
+    this.newRg = new Rg(this.editRgForm);
+    console.log(this.newRg);
+  }
+
+}
