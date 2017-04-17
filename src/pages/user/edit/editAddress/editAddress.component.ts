7b25db09d908f834d21d4c1d88ec9f2fa7f157bc
Hugo Neves de Carvalho <hugo-nc@hotmail.com>
2017-04-17 01:22:14 -0300

    Updating address form connections
    Signed-off-by: Hugo Neves de Carvalho <hugo-nc@hotmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- src/pages/user/edit/editAddress/editAddress.component.ts
+++ src/pages/user/edit/editAddress/editAddress.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component,Input } from "@angular/core";
 import { NavController } from "ionic-angular"
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { UserProfile, Address } from '../../../../models/userProfile';
@@ -6,30 +6,14 @@ import { ValidateEmail, ValidatePassword } from '../../../controller/custom-vali
 import { Http } from '@angular/http'
 
 @Component({
-  selector: "edit",
+  selector: "editAddress",
   templateUrl: 'editAddress.component.html'
 })
 
 //Class used to handle with user address informations.
 export class EditAddress {
-  editAddressForm: FormGroup;
-  newAddress: Address;
 
-  //Form responsible to register informations related to address.
-  constructor(public navCtrl: NavController, formBuilder: FormBuilder, private http: Http) {
-    this.editAddressForm = formBuilder.group({
-      'cep' : [null, Validators.compose([Validators.required])],
-      'city' : [null, Validators.compose([Validators.required])],
-      'state' : [null, Validators.compose([Validators.required])],
-      'neighbourhood' : [null, Validators.compose([Validators.required])],
-      'number' : [null, Validators.compose([Validators.required])],
-      'complement' : [null, Validators.compose([Validators.required])]
-    });
-  }
+  @Input('group')
+  editAddress: FormGroup;
 
-  //Method responsible to attribute the data of address to the user profile.
-  submitForm(value: any):void{
-    this.newAddress = new Address(this.editAddressForm)
-    console.log(this.newAddress)
-  }
 }
