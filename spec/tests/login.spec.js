a1ac3defa948eac1c72db9645776b89cd11ab92f
Vitor Menezes <torandoing@gmail.com>
2017-04-17 01:25:40 -0300

    Created sign in submit button disabled test

--- spec/tests/login.spec.js
+++ spec/tests/login.spec.js
@@ -5,13 +5,24 @@ var browser = new Browser({
 });
 
 describe("Sign In Tests", function(){
+  describe("Shouldn't show submit button if email is invalid", function () {
+    it("Shouldn't allow signin with invalid information", function () {
+      browser.visit(url, function(){
+        browser.fill("#email input[name='email']", "test")
+               .fill("#password input[name='password']", "Teste123");
+      var submit = byId('sign-in-button');
+      expect(submit.isEnabled()).toBe(false);
+      });
+    });
+  });
+
   describe("Sign In Failure state", function () {
     it("Shouldn't allow signin with invalid information", function () {
       browser.visit(url, function(){
-        browser.fill("#email input[name='email']", "uhuu@gmail.com")
+        browser.fill("#email input[name='email']", "test@gmail.com")
                .fill("#password input[name='password']", "Teste123")
                .pressButton("#sign-in-button button[type=submit]");
-        expect(browser.html('body')).toContain("Login Error!");
+      expect(browser.html('body')).toContain("Login Error!");
       });
     });
   });
