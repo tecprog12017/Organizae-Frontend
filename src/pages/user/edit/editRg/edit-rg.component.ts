5965b25e3bd1cfea9f85a6fbd028a4bdfa1757e6
Luis Gustavo <luis.gustavo3013@hotmail.com>
2017-04-22 05:26:39 -0300

    Updated documentaion information forms
    Signed-off-by: Hugo Neves de Carvalho <hugo-nc@hotmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- src/pages/user/edit/editRg/edit-rg.component.ts
+++ src/pages/user/edit/editRg/edit-rg.component.ts
@@ -4,15 +4,25 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { UserProfile, Rg } from '../../../../models/user-profile';
 import { ValidateEmail } from '../../../../controller/custom-validations'
 import { Http } from '@angular/http'
-import { EditAddress } from '../../../../pages/user/edit/editAddress/edit-address.component'
+import { StatesService } from '../../../../pages/user/edit/editAddress/states.service'
+import { UserTokenSession } from '../../signIn/user-token-session.service'
 
 @Component({
   selector: "edit-rg",
-  templateUrl: 'edit-rg.component.html'
+  templateUrl: 'edit-rg.component.html',
+  providers: [StatesService]
 })
 
 //Class used to deal with rg data.
 export class EditRg {
   @Input('group')
   editRg: FormGroup
+  userToken: Object
+  states = []
+
+  constructor(private userTokenSession: UserTokenSession, private statesService: StatesService){
+      this.userToken = this.userTokenSession.getToken()
+      this.states = statesService.getStates()
+  }
+
 }
