708eaf81116c5c81df4c2d4ce51f1ad9143a4f13
Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>
2017-04-22 05:29:16 -0300

    Created service for userToken
    Signed-off-by: Hugo Neves de Carvalho <hugo-nc@hotmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

---
+++ src/pages/user/signIn/user-token-session.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UserTokenSession {
+  userToken: Object;
+
+  constructor(){
+  }
+
+  setToken(userToken: Object){
+    this.userToken = userToken;
+  }
+
+  getToken(){
+    return this.userToken;
+  }
+
+}
