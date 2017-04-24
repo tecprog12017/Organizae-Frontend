5f4433d6c3bb424e71dd8f6c4ce429e5573dbab7
lucaaas <lucasoliveira2306@gmail.com>
2017-04-24 14:30:14 -0300

    Updated configurations files.

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-var express = require(‘express’);
-var path = require(‘path’)
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, “www”)));
-app.set(‘port’, process.env.PORT || 3000);
-app.listen(app.get(‘port’), function() {
- console.log(“listening to Port”, app.get(“port”));
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 8080);
+app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.use(express.static("www")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
+
+// Initialize the app.
+app.listen(app.get('port'), function () {
+console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
+});
 });
