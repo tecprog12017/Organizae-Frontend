73a64a09df3295f0e62cc5ec4658cc967e5ab4d2
lucaaas <lucasoliveira2306@gmail.com>
2017-04-24 10:45:22 -0300

    Changed the configuration file.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require('express'),
-    app = express();
-    app.use(express.static('www'));
-    app.set('port', process.env.PORT || 5000);
-    app.listen(app.get('port'), function () {
-        console.log('Express server listening on port ' + app.get('port'));
-    });
+var express = require(‘express’);
+var path = require(‘path’)
+var app = express();
+
+app.use(express.static(path.resolve(__dirname, “www”)));
+app.set(‘port’, process.env.PORT || 3000);
+app.listen(app.get(‘port’), function() {
+ console.log(“listening to Port”, app.get(“port”));
+});
