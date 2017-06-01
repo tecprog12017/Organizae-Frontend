5f305941aa4d0898e8523b7e3eb99a2aa81f4c64
Vitor Menezes <torandoing@gmail.com>
2017-06-01 10:00:06 -0300

    Created main page for account settings

---
+++ src/pages/user/accountSettings/settingsMain/settings-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DeleteUser } from '../deleteUser/delete-user.component';
+import { UserHome } from '../../userHome/user-home.component';
+
+@Component({
+  selector: "settings-main",
+  templateUrl: 'settings-main.component.html'
+})
+
+export class SettingsMain {
+
+}
