f839b7bb5ee0fc2f9c6ea4a19091526c1448ad4e
fabiolamfleury <fabiola.mfleury@gmail.com>
2017-06-02 15:17:39 -0300

    Creating link to get user enterprises from backend

--- src/pages/enterprise/listEnterprises/list-enterprises.component.ts
+++ src/pages/enterprise/listEnterprises/list-enterprises.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Http } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/Rx';
 
 import { UserTokenSession } from '../../user/signIn/user-token-session.service';
 import { RegisterEnterprise } from '../registerEnterprise/register-enterprise.component';
@@ -12,7 +14,21 @@ import { RegisterEnterprise } from '../registerEnterprise/register-enterprise.co
 export class ListEnterprises {
   newEnterprise = RegisterEnterprise;
   constructor(public navCtrl: NavController, private http: Http, public userTokenSession: UserTokenSession) {
+    // Setting user email as parameter to pass user to backend
+    let params = new URLSearchParams();
+    params.set('user', this.userTokenSession.getToken());
 
+    // Getting enterprises that belong to logged user from backend
+    this.http.get('http://localhost:3000/api/enterprises/consult-enterprises', { search: params }).map(res => res.json())
+    .subscribe( res => {
+      if (res.status == 200){
+        //user has enterprises
+      }
+      else {
+        //user doesnt have enterprises
+      }
+
+    });
 
   }
 }
