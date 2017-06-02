760bfa1fc3189fb481aca89de9721b78d2bf0ffe
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-06-02 15:09:28 -0300

    Adding new attributes to enterprise model.

--- src/models/enterprise.ts
+++ src/models/enterprise.ts
@@ -4,9 +4,11 @@ import { Address } from './user-profile';
 export class Enterprise {
   name: string;
   cnpj: string;
+  oldCnpj: string;
   occupationArea: string;
   address: Address;
   owner: Object;
+  confirmationPassword: string;
 
   constructor (form: FormGroup) {
     this.name = form.get('name').value;
@@ -15,4 +17,14 @@ export class Enterprise {
     this.address = new Address(form);
     this.owner = null;
   }
+
+  public setEditEnterprise(form: FormGroup) {
+    this.name = form.get('name').value;
+    this.cnpj = form.get('cnpj').value;
+    this.occupationArea = form.get('occupationArea').value;
+    this.confirmationPassword = form.get('confirmationPassword').value;
+    this.address = new Address(form);
+    this.owner = null;
+  }
+
 }
