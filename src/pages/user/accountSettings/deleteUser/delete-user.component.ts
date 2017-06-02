da1afd4b398678dbdfcac7a6faa41ece71f18eb9
Renatafsouza <renata.rfsouza@gmail.com>
2017-06-02 09:22:58 -0300

    Creating method submitForm

--- src/pages/user/accountSettings/deleteUser/delete-user.component.ts
+++ src/pages/user/accountSettings/deleteUser/delete-user.component.ts
@@ -6,9 +6,12 @@ import { Http } from '@angular/http';
 import { AlertController } from 'ionic-angular';
 import { UserTokenSession } from '../../signIn/user-token-session.service'
 import { ValidateEmail, ValidatePassword } from '../../../../controller/custom-validations';
+import { UserHome } from '../../userHome/user-home.component';
 
 import 'rxjs/add/operator/map';
 import * as  jwt from 'jwt-simple/lib/jwt';
+
+//jesus14320@hotmail.com
 
 @Component({
   selector: "delete-user",
@@ -20,6 +23,7 @@ export class DeleteUser {
   deleteUserForm : FormGroup
   currentUser: UserProfile;
   userToken: Object;
+  secret = 'tecprog-2017/01';
 
 //Form used for the member to delete own account
 constructor(public navCtrl: NavController, formBuilder: FormBuilder, private http: Http,
@@ -29,4 +33,32 @@ constructor(public navCtrl: NavController, formBuilder: FormBuilder, private htt
     'password': [null, Validators.compose([Validators.required, ValidatePassword()])],
   });
  }
+
+ //submit used to authenticate the user in system
+ submitForm(value: any):void{
+   this.http.post('http://localhost:3000/api/UserProfiles/deleteAccount', this.deleteUserForm.value)
+    .map(res => res.json())
+    .subscribe(token => {
+      if(token.status != 400){
+        this.userToken = jwt.decode(token.status, this.secret);
+        this.navCtrl.setRoot(UserHome, { }, {animate: true, direction: 'forward'});
+        this.userTokenSession.setToken(this.userToken);
+      }
+      else{
+        this.showDeleteUserError();
+      }
+      }
+    );
+  }
+
+  //Used to show the user if an error ocurred during his access attempt
+  showDeleteUserError () {
+    let alert = this.alertCtrl.create({
+      title: 'Password Incorrect!',
+      subTitle: `There was an error! Check your password's input.`,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 }
