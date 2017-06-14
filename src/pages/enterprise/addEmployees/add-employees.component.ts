5738d9341672dc04be6ac3e6ce7e6d7c69c45b90
Luis Gustavo <luis.gustavo3013@hotmail.com>
2017-06-14 11:50:13 -0300

    Upgraded methods to register employee at enterprise
    Signed-off-by: Hugo Neves de Carvalho <hugo-nc@hotmail.com>
    Signed-off-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- src/pages/enterprise/addEmployees/add-employees.component.ts
+++ src/pages/enterprise/addEmployees/add-employees.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { Http, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/Rx';
 
 import { UserProfile } from '../../../models/user-profile/';
 import { UserTokenSession } from '../../user/signIn/user-token-session.service';
+import { Enterprise } from '../../../models/enterprise';
 
 @Component({
   templateUrl: 'add-employees.component.html'
@@ -13,9 +14,14 @@ import { UserTokenSession } from '../../user/signIn/user-token-session.service';
 
 export class AddEmployees {
   allUsers: Array<Object>;
+  selectedUsers: Array<String>;
+  currentEnterprise: Enterprise;
 
-  constructor(public navCtrl: NavController, private http: Http, public userTokenSession: UserTokenSession) {
+  constructor(public navCtrl: NavController, private http: Http, public userTokenSession: UserTokenSession,
+              public navParams: NavParams) {
     this.allUsers = null;
+    this.selectedUsers = [];
+    this.currentEnterprise = navParams.get('currentEnterprise');
 
   }
 
@@ -32,8 +38,38 @@ export class AddEmployees {
       resolve(this.allUsers);
       });
     });
+  }
 
+  addEmployee(userEmail, checkBoxEvent){
+    if(checkBoxEvent.checked){
+      this.selectedUsers.push(userEmail);
+    }else{
+      let indexOfUserEmail = this.selectedUsers.indexOf(userEmail);
+      this.selectedUsers.splice(indexOfUserEmail, 1);
+    }
+  }
 
+  registerEmployees(){
+    console.log(this.currentEnterprise.owner, this.currentEnterprise.name);
+
+    let selectEnterprise = {
+                            "owner":{
+                                "email":this.currentEnterprise.owner
+                                },
+                            "name": this.currentEnterprise.name
+                          }
+
+    let selectEmployees = {
+                            "employees": this.selectedUsers
+                          }
+
+    let data = new URLSearchParams();
+    data.append('enterprise', JSON.stringify(selectEnterprise));
+    data.append('users', JSON.stringify(selectEmployees));
+
+    this.http.post('http://localhost:3000/api/enterprises/add-employee', data).subscribe();
+
+    this.http.post('http://localhost:3000/api/UserProfiles/add-enterprise', data).subscribe();
   }
 
 
